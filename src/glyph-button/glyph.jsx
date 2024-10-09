51f2ea6e1aeda0dbbf21fbd2372b6513c6b55374
Bohak Oleh <eddrfgthyjn@gmail.com>
2024-10-09 19:06:46 +0300

    .

--- src/glyph-button/glyph.jsx
+++ src/glyph-button/glyph.jsx
@@ -6,7 +6,7 @@ const GLYPHS =
 
 const App = () => {
   const [text, setText] = useState('Join the course');
-  const [speed, setSpeed] = useState(0.2);
+  const [speed, setSpeed] = useState(0.1);
   const [explode, setExplode] = useState(false);
 
   useEffect(() => {
